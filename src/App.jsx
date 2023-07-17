83c705464e520d83978e769ead1e99f074bc92ac
Gabriel <gfcontroladoriageral@gmail.com>
2023-07-17 14:10:49 -0300

    Todo Stylezed

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,75 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import Todo from './components/Todo'
+import TodoForm from './components/TodoForm'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "Criar Funcionalidade X no sistema",
+      category: "Trabalho",
+      isCompleted: false,
+      button: "Completar"
+    },
+    {
+      id: 2,
+      text: "Ir para a academia",
+      category: "Pessoal",
+      isCompleted: false,
+      button: "Completar"
+    },
+    {
+      id: 3,
+      text: "Estudar React",
+      category: "Estudos",
+      isCompleted: false,
+      button: "Completar"
+    }
+  ])
+  const addTodo = (text, category) => {
+    const newTodo = [
+      ...todos,
+      {
+        id: Math.floor(Math.random() * 10000),
+        text,
+        category,
+        isCompleted: false
+      },
+    ];
+    setTodos(newTodo);
+  }
+  const removeTodo = (id) => {
+    const newTodo = [...todos];
+    const filterTodo = newTodo.filter(
+      (todos) => todos.id !== id ? todos : null
+    );
+    setTodos(filterTodo)
+  }
+  const completeTodo = (id) => {
+    const newTodo = [...todos];
+    newTodo.forEach((todo) => {
+      if (todo.id === id) {
+        todo.isCompleted = !todo.isCompleted;
+        todo.isCompleted ? (todo.button = "Refazer") : (todo.button = "Completar");
+        setTodos(newTodo)
+      }
+    });
+  };
+    
+  
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <div className="card">
-        
-      </div>
-      
-    </>
+   <div className="app">
+    <h1>Lista de Tarefas</h1>
+    <div className="listTodo">
+      {todos.map((todo) => (
+        <Todo key={todo.id} todo={todo} removeTodo={removeTodo} completeTodo={completeTodo} />
+      ))}
+    </div>
+    <TodoForm addTodo={addTodo}/>
+   </div>
   )
 }
 
